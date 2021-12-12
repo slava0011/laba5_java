897e23d06487a6b4e413257d45e36d4c2e0e5477
slava0011 <mr.slawa2003@mail.ru>
2021-12-13 00:20:32 +0300

    added default variables method

--- src/bsu/rfe/java/group7/lab5/Kondrashov/varC/MainFrame.java
+++ src/bsu/rfe/java/group7/lab5/Kondrashov/varC/MainFrame.java
@@ -1,5 +1,38 @@
 package bsu.rfe.java.group7.lab5.Kondrashov.varC;
 
-public class MainFrame {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private boolean fileLoaded = false;
+    private JFileChooser fileChooser = null;
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+    private JCheckBoxMenuItem reformCoordinateItem;
+    private GraphicsDisplay display = new GraphicsDisplay();
+    private JCheckBoxMenuItem showGridItem;
+
+    
 }
